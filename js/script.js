b1f424436b89447dfeeaebf8a6d4647bfa29e481
mistershashanksingh <mistershashanksingh@gmail.com>
2023-07-08 14:59:47 +0530

    remove professional wheel and dimension with technology icon

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
-//==============sticky navbar background==============
-window.onscroll = () => {
-let header = document.querySelector('.header');
 
-header.classList.toggle('sticky', window.scrollY > 100)
-}
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+
+    // ---------------scroll section--------------
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            // action navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        }
+    });
+
+    //==============sticky navbar background==============
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+};
 
 //---------------color mode(dark & light)--------------
 let darkModeIcon = document.querySelector('#darkMode-icon');
