32fc9783de8a8c98b0b9d9bac7d017c6219e019f
mistershashanksingh <mistershashanksingh@gmail.com>
2023-07-12 23:29:25 +0530

    add animation in mobile navbar && improve in code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// --------------- manu section navbar ------------------
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+// ---------------scroll section active link--------------
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -27,6 +37,10 @@ window.onscroll = () => {
     let header = document.querySelector('.header');
 
     header.classList.toggle('sticky', window.scrollY > 100)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active')
+
 };
 
 //---------------color mode(dark & light)--------------
