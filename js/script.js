545ed62f95f38a2ff428d86871b7d873593ea39e
mistershashanksingh <mistershashanksingh@gmail.com>
2023-11-20 22:23:09 +0530

    minor changes

--- js/script.js
+++ js/script.js
@@ -82,3 +82,59 @@ ScrollReveal().reveal('.home-img, .about-img, .contact, .skills-row', { origin: 
 ScrollReveal().reveal('.home-content h2,', { origin: 'left' })
 ScrollReveal().reveal('.education-content, .about-content', { origin: 'right' })
 
+// ___________________readmore content_______________________________
+
+function toggleParagraph() {
+    var paragraph = document.querySelector('.expandable-paragraph');
+    var button = document.querySelector('.read-more');
+  
+    if (paragraph.classList.contains('expanded')) {
+        paragraph.style.maxHeight = '2rem';
+        paragraph.classList.remove('expanded');
+        button.classList.remove('expanded');
+        button.textContent = 'Read More';
+      } else {
+        paragraph.style.maxHeight = 'none';
+        paragraph.classList.add('expanded');
+        button.classList.add('expanded');
+        button.textContent = 'Read Less';
+      }
+  }
+  
+
+
+// _________________________________container typing effect @home__________________________________
+const dynamicText = document.querySelector(".second");
+const words = ["Grad Student", "Open Source", "Tech Blogging"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect();
+
+// sub - contact form 
